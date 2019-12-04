33b1e400dfb323f03302ee2d28baf919903d57f0
Nikhil Limaye <nikhillimaye10@gmail.com>
2019-12-03 22:42:02 -0800

    Cleaned up project

--- backend/spring-jpa-mysql/src/main/java/com/testproject/springsecurityjpamysql/dao/UserProfileDao.java
+++
@@ -1,9 +0,0 @@
-package com.testproject.springsecurityjpamysql.dao;
-
-import com.testproject.springsecurityjpamysql.model.UserProfile;
-
-public interface UserProfileDao {
-	
-	public void insert(UserProfile user);
-	
-}
