28a137764a2b358fb115b33d7622262d2efc2127
Arman Pathan <arman.pathan@sjsu.edu>
2019-12-17 23:24:53 -0800

    Update route to point to backend instance
    Update route to point to backend instance

--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -1,2 +1,3 @@
-export const API_ENDPOINT = "http://localhost:5000";
+export const API_ENDPOINT =
+  "http://Openhomebackend-env.5gxpn3azqy.us-west-1.elasticbeanstalk.com:5000";
 export const IMAGE_PATHS = "components/images/";
